b6a03e144453536e20158ee2cc91a2fe8d8e15f5
Fernando Córdova <65390031+fernandoCordova@users.noreply.github.com>
2022-06-15 05:42:04 -0400

    Update ApiConfiguracion.php

--- config/ApiConfiguracion.php
+++ config/ApiConfiguracion.php
@@ -3,6 +3,6 @@ class ApiConfiguracion
 {
     public function obtenerClaveApi()
     {
-        return 'RGAPI-936f5a20-fab5-46d8-9321-93b563c468a8';
+        return 'RGAPI-xxxx-x-xxxx--xxx';
     }
 }
